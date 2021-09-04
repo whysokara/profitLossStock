3391df504d590f92fd3f2f0954535daafa20d523
Kara <kara_himanshu@yahoo.com>
2021-09-05 02:33:00 +0530

    Logic Added

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 var initialPrice = document.querySelector("#initialPrice");
 var stocksQuantity = document.querySelector("#stocksQuantity");
-var finalPrice = document.querySelector("#finalPrice");
+var finalPrice = document.querySelector("#currentPrice");
 
 var submitBtn = document.querySelector("#submitBtn");
 var output = document.querySelector("#output");
 
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stocksQuantity.value);
+  var curr = Number(finalPrice.value);
+
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
 function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
-    var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
+    var loss = ((initial - current) * quantity).toFixed(2);
+    var lossPercentage = ((loss / initial) * 100).toFixed(2);
 
-    console.log(`The loss is ${loss} and percent is ${lossPercentage}%`);
+    showOutput(`The loss is ${loss} and percent is ${lossPercentage}%`);
   } else if (initial < current) {
-    var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
+    var profit = ((current - initial) * quantity).toFixed(2);
+    var profitPercentage = ((profit / initial) * 100).toFixed(2);
 
-    console.log(`The profit is ${profit} and percent is ${profitPercentage}%`);
+    showOutput(`The profit is ${profit} and percent is ${profitPercentage}%`);
   } else {
-    console.log("No pain, No gain");
+    showOutput("No pain, No gain");
   }
+}
+
+function showOutput(message) {
+  output.innerHTML = message;
 }
